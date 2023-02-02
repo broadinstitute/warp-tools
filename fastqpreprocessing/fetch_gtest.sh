4dd6351f5420c603f92115d01fd9967150e38c1d
Fred Douglas <fred.e.douglas1231@gmail.com>
2023-02-02 15:06:27 -0600

    remove backticks from echo

--- fastqpreprocessing/fetch_gtest.sh
+++ fastqpreprocessing/fetch_gtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d gtest ]; then
-  echo "gtest directory already present. If `make test` doesn't work, delete gtest and run this script again."
+  echo "gtest directory already present. If 'make test' doesn't work, delete gtest and run this script again."
 else
   wget "https://github.com/google/googletest/archive/refs/tags/v1.13.0.tar.gz" && \
   tar xf v1.13.0.tar.gz && \
@@ -11,5 +11,5 @@ else
   mv googletest-1.13.0/googletest/src/* gtest/src/ && \
   mv googletest-1.13.0/googlemock/src/* gtest/src/ && \
   rm -f gtest/src/gmock_main.cc && \
-  echo "gtest acquired. Running `make test` in this directory should now work."
+  echo "gtest acquired. Running 'make test' in this directory should now work."
 fi
