bbb11a33f57e3cf58e3ad6022d28a776f61ed58d
Fred Douglas <43351173+fredlas@users.noreply.github.com>
2023-02-09 22:49:07 -0600

    Split TagSort out into its own directory (#19)
    TagSort has until now been mixed in with fastqpreprocessing. Some previous refactoring (#6) ensured that TagSort really is completely independent from fastqpreprocessing, but it was still sitting in the same directory. There will now be a TagSort directory sibling to fastqpreprocessing, with its own Makefile. The final overall warp-tools Docker image still winds up with all of the fastq and TagSort binaries in the same /usr/local/bin/ directory.
    
    Fixes #11.

--- fastqpreprocessing/fetch_and_make_dep_libs.sh
+++ fastqpreprocessing/fetch_and_make_dep_libs.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-HTSLIBVERSION="1.16"
-
 wget https://github.com/khajoue2/libStatGen/archive/refs/tags/v1.0.15.broad.tar.gz && \
 tar -zxvf v1.0.15.broad.tar.gz && \
 mv libStatGen-1.0.15.broad libStatGen && \
 make -C libStatGen && \
-
-wget "https://github.com/samtools/htslib/releases/download/$HTSLIBVERSION/htslib-$HTSLIBVERSION.tar.bz2" && \
-tar -jxvf "htslib-$HTSLIBVERSION.tar.bz2" && \
-mv "htslib-$HTSLIBVERSION" htslib && \
-cd htslib && \
-./configure --disable-libcurl && \
-make && \
-cd .. && \
 
 wget http://www.cs.unc.edu/Research/compgeom/gzstream/gzstream.tgz && \
 tar -xvf gzstream.tgz && \
