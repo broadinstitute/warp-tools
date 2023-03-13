69f1160583e944e9d2c97220e3bb64759a9f294c
Kevin Palis <palis@broadinstitute.org>
2023-03-13 18:03:45 -0400

    Adding pytools, optimizing dockerfile, adding requirements file, and optimizing docker_build.

--- Dockerfile
+++ Dockerfile
@@ -7,19 +7,18 @@ LABEL maintainer="Broad Institute Pipeline Development Team <pipeline-developmen
   software="warp-tools  v.1.0.0" \
   description="A collection of tools for WARP pipelines."
 
+COPY . /warptools
 
-RUN apt-get update && apt-get upgrade -y && apt-get install -y libhdf5-dev vim apt-utils liblzma-dev libbz2-dev
-RUN mkdir -p /sctools
+RUN set -eux; \
+    pip install --upgrade pip; \
+    apt-get update && apt-get upgrade -y && apt-get install -y libhdf5-dev vim apt-utils liblzma-dev libbz2-dev && \
+    mkdir -p /warptools && \
+    cd /warptools/fastqpreprocessing && ./fetch_and_make_dep_libs.sh && make && cp /warptools/fastqpreprocessing/bin/* /usr/local/bin/ && \
+    cd /warptools/TagSort && ./fetch_and_make_dep_libs.sh && make && cp /warptools/TagSort/bin/* /usr/local/bin/ && \
+    pip install loompy anndata \
+    pip3 install -r requirements.txt
 
-COPY . /sctools
-
-RUN cd /sctools/fastqpreprocessing && ./fetch_and_make_dep_libs.sh && make && cp /sctools/fastqpreprocessing/bin/* /usr/local/bin/
-
-RUN cd /sctools/TagSort && ./fetch_and_make_dep_libs.sh && make && cp /sctools/TagSort/bin/* /usr/local/bin/
-
-RUN pip install loompy anndata
-
-WORKDIR usr/local/bin/sctools
+WORKDIR /warptools
 
 # Set tini as default entrypoint
 # ENTRYPOINT ["/usr/bin/tini", "--"]
--- docker_build.sh
+++ docker_build.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-set -e
+#fail-fast
+set -e -x
 
 # Update version when changes to Dockerfile are made
 DOCKER_IMAGE_VERSION=1.0.0
 TIMESTAMP=$(date +"%s")
-DIR=$(cd $(dirname $0) && pwd)
+DIR=$(cd "$(dirname "$0")" && pwd)
+TAG=$1
+CACHING=$2
 
 # Registries and tags
 GCR_URL="us.gcr.io/broad-gotc-prod/warp-tools"
 
-# warp-tools version
-WARP_TOOLS_VERSION="v0.3.15"
 
 # Necessary tools and help text
 TOOLS=(docker gcloud)
-HELP="$(basename "$0") [-h|--help] [-v|--version] [-t|tools] -- script to build the warp-tools image and push to GCR & Quay
-
+HELP="$(basename "$0") [-h|--help] [-t|tools] [TAG] [CACHING] -- script to build the image and push to GCR
 where:
     -h|--help Show help text
-    -v|--version Version of Samtools to use (default: $WARP_TOOLS_VERSION)
     -t|--tools Show tools needed to run script
+    TAG : Set a fixed tag string (this will override our tagging convention). Useful for development or on CI/CD to reduce wasted images
+    CACHING : Set to ON to enable caching. Any other value or simply not setting it will default to no-cache builds.
     "
 
 function main(){
@@ -34,11 +35,6 @@ function main(){
     do 
     key="$1"
     case $key in
-        -v|--version)
-        WARP_TOOLS_VERSION="$2"
-        shift
-        shift
-        ;;
         -h|--help)
         echo "$HELP"
         exit 0
@@ -52,12 +48,20 @@ function main(){
         ;;
     esac
     done
-    
-    IMAGE_TAG="$DOCKER_IMAGE_VERSION-$WARP_TOOLS_VERSION-$TIMESTAMP"
+    if [ -v TAG ]; 
+    then
+        IMAGE_TAG="$TAG"
+    else
+        IMAGE_TAG="$DOCKER_IMAGE_VERSION-$TIMESTAMP"
+    fi
 
     echo "building and pushing GCR Image - $GCR_URL:$IMAGE_TAG"
-    docker build --no-cache -t "$GCR_URL:$IMAGE_TAG" \
-        --build-arg WARP_TOOLS_VERSION="$WARP_TOOLS_VERSION" "$DIR" 
+
+    if [[ "$CACHING" == "ON" ]]; then
+        docker build -t "$GCR_URL:$IMAGE_TAG" "$DIR"
+    else
+        docker build -t "$GCR_URL:$IMAGE_TAG" --no-cache "$DIR"
+    fi
     docker push "$GCR_URL:$IMAGE_TAG"
 
     echo -e "$GCR_URL:$IMAGE_TAG" >> "$DIR/docker_versions.tsv"
