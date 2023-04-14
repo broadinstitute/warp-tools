437bad09414f7792039f1ac032e4e0475e731bbc
Aseel Awdeh <awdeh@broadinstitute.org>
2023-04-14 16:40:27 -0400

    hardcoded nthreads to 1000 when nthreads returned is greater than 1000 -- this is an issue with large files

--- fastqpreprocessing/src/fastqprocess.cpp
+++ fastqpreprocessing/src/fastqprocess.cpp
@@ -116,6 +116,8 @@ int main(int argc, char** argv)
   InputOptionsFastqProcess options = readOptionsFastqProcess(argc, argv);
   // number of output bam files, and one writer thread per bam file
   int num_writer_threads = get_num_blocks(options);
+  // hardcoded this to 1000 in case of large files
+  num_writer_threads =  (num_writer_threads > 1000) ? 1000 : num_writer_threads;
 
   g_barcode_length = options.barcode_length;
   g_umi_length = options.umi_length;
@@ -124,6 +126,6 @@ int main(int argc, char** argv)
              num_writer_threads, options.output_format,
              options.I1s, options.R1s, options.R2s, options.R3s, options.sample_id,
              fillSamRecord, barcodeGetter, outputHandler);
-             
+
   return 0;
 }
