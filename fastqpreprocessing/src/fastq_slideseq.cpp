f7c69b16edda12d332e3ba0f08c31290eee31773
Aseel Awdeh <awdeh@broadinstitute.org>
2023-04-19 16:52:01 -0400

    add number threads to slideseq

--- fastqpreprocessing/src/fastq_slideseq.cpp
+++ fastqpreprocessing/src/fastq_slideseq.cpp
@@ -11,6 +11,8 @@ int main(int argc, char** argv)
   INPUT_OPTIONS_FASTQ_READ_STRUCTURE options = readOptionsFastqSlideseq(argc, argv);
   // number of output bam files, and one writer thread per bam file
   int num_writer_threads = get_num_blocks(options);
+  // hardcoded this to 1000 in case of large files
+  num_writer_threads =  (num_writer_threads > 1000) ? 1000 : num_writer_threads;
 
   std::vector<std::pair<char, int>> g_parsed_read_structure = parseReadStructure(options.read_structure);
 
