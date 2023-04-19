de5851e8818a5969558ec506f33ad7989d7591cf
Aseel Awdeh <awdeh@broadinstitute.org>
2023-04-19 15:49:30 -0400

    fix barcode length

--- fastqpreprocessing/src/fastq_common.cpp
+++ fastqpreprocessing/src/fastq_common.cpp
@@ -336,7 +336,7 @@ void fillSamRecord(SamRecord* samRecord, FastQFile* fastQFileI1,
   }
   else if (has_R3_file_list)
   {
-      g_barcode_length std::get<1>(g_parsed_read_structure[0]);
+      g_barcode_length = std::get<1>(g_parsed_read_structure[0]);
       std::cout << "BARCODE LENGTH FOR ATAC " << g_barcode_length << "\n";
       
       if (strcmp(orientation.c_str(), "LAST_BP") == 0)
