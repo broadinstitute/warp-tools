0b915c7c42c8a9180fe72659f320672814a28a04
aawdeh <awdeh@broadinstitute.org>
2023-04-28 15:31:42 -0400

    write both cb and cr barcodes to read name

--- fastqpreprocessing/src/fastq_common.cpp
+++ fastqpreprocessing/src/fastq_common.cpp
@@ -130,18 +130,21 @@ void writeFastqRecordATAC(ogzstream& r1_out, ogzstream& r2_out, ogzstream& r3_ou
 
   std::string write_cb_barcode = "";
   if (!cb_barcode.empty())
-    write_cb_barcode = ":CB_" + cb_barcode; 
+    write_cb_barcode = cb_barcode + ":CB:"; 
   
   //R1
-  r2_out << "@" << sam->getReadName() << ":CR_" << cr_barcode << write_cb_barcode
+  r2_out << "@" << write_cb_barcode <<
+          << sam->getReadName() << ":CR:" << cr_barcode
           << "\n" << cr_barcode
           << sam->getString("UR") << "\n+\n" << sam->getString("CY") << sam->getString("UY") << "\n";
   //R2
-  r1_out << "@" << sam->getReadName() << ":CR_" << cr_barcode << write_cb_barcode
+  r1_out << "@" << write_cb_barcode <<
+          << sam->getReadName() << ":CR:" << cr_barcode
           << "\n" << sam->getSequence() << "\n+\n"
           << sam->getQuality() << "\n";
   //R3
-  r3_out << "@" << sam->getReadName() << ":CR_" << cr_barcode << write_cb_barcode
+  r3_out << "@" << write_cb_barcode <<
+          << sam->getReadName() << ":CR:" << cr_barcode
           << "\n" << sam->getString("RS").c_str() << "\n+\n"
           << sam->getString("RQ").c_str() <<  "\n";
   
