cfd24f99e49a2fd614250ab393c79626918fbf65
ekiernan <ekiernan@broadinstitute.org>
2023-07-19 14:39:23 -0400

    defined anti sense metric variables

--- tools/TagSort/src/metricgatherer.h
+++ tools/TagSort/src/metricgatherer.h
@@ -131,7 +131,9 @@ private:
 
   // alignment location information
   int reads_mapped_exonic_ = 0;
+  int reads_mapped_exonic_as_ = 0;
   int reads_mapped_intronic_ = 0;
+  int reads_mapped_intronic_as_ = 0;
   int reads_mapped_utr_ = 0;
 
   // in future we can implement this when we have a gene model
