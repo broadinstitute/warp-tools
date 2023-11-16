b9e9099b980cca8a783d677dc9b1cfcdef31a08a
aawdeh <awdeh@broadinstitute.org>
2023-11-16 14:44:33 -0500

    Added print statements and rephrased if statement

--- tools/fastqpreprocessing/src/fastqprocess.cpp
+++ tools/fastqpreprocessing/src/fastqprocess.cpp
@@ -5,12 +5,22 @@ int main(int argc, char** argv)
 {
   InputOptionsFastqProcess options = readOptionsFastqProcess(argc, argv);
   
-  // number of output bam files, and one writer thread per bam file
-  int num_writer_threads = get_num_blocks(options);
-  // hardcoded this to 1000 in case of large files
-  num_writer_threads =  (num_writer_threads > 1000) ? 1000 : num_writer_threads;
+  int num_writer_threads = 1;
+  if (options.num_output_file != -1) {
+    std::cout<<"options.num_output_file != -1\n";
+    num_writer_threads = options.num_output_file;
+  }
+  else {
+    std::cout<<"ELSE\n";
+    // number of output bam files, and one writer thread per bam file
+    num_writer_threads = get_num_blocks(options);
+    // hardcoded this to 1000 in case of large files
+    num_writer_threads =  (num_writer_threads > 1000) ? 1000 : num_writer_threads;
+  }
 
-  num_writer_threads= (options.num_output_files != -1) ? options.num_output_files : num_writer_threads;
+  std::cout<<num_writer_threads<<"\n";
+  // num_writer_threads= (options.num_output_files != -1) ? options.num_output_files : num_writer_threads;
+  std::cout<<options.num_output_files<<"\n";
 
   // added this for consistency with other code
   std::vector<std::pair<char, int>> g_parsed_read_structure = parseReadStructure(options.read_structure);
