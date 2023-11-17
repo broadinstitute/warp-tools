a2193bd927bc8d3b6010ee7850d9c31dd4429dce
ekiernan <ekiernan@broadinstitute.org>
2023-11-17 15:28:23 -0500

    updated docker with timestamp for latest tagsort changse

--- tools/docker_build.sh
+++ tools/docker_build.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 # Update version when changes to Dockerfile are made
-DOCKER_IMAGE_VERSION=1.0.8
+DOCKER_IMAGE_VERSION=1.0.9
 TIMESTAMP=$(date +"%s")
 DIR=$(cd "$(dirname "$0")" && pwd)
 TAG=$1
