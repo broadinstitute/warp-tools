99fbaf9aac8e1089f1255a0c29f602f035eb6aa3
ekiernan <ekiernan@broadinstitute.org>
2024-03-01 10:03:57 -0500

    lowercase internal variables

--- tools/scripts/combine_shard_metrics.py
+++ tools/scripts/combine_shard_metrics.py
@@ -26,18 +26,18 @@ def merge_matrices(summary_file, align_file, cell_reads, counting_mode):
     print(counting)
     print("Calcuating metrics")
     merge_pv[f"Reads Mapped to {counting}: Unique {counting}*n_reads"] = merge_pv[f'Reads Mapped to {counting}: Unique {counting}']*merge_pv['Number of Reads']
-    Sum_reads_mapped_unique_gene=merge_pv[f"Reads Mapped to {counting}: Unique {counting}*n_reads"].sum()
-    Total_reads_mapped_unique_gene=Sum_reads_mapped_unique_gene/n_reads
+    sum_reads_mapped_unique_gene=merge_pv[f"Reads Mapped to {counting}: Unique {counting}*n_reads"].sum()
+    total_reads_mapped_unique_gene=Sum_reads_mapped_unique_gene/n_reads
     merge_pv["Reads Mapped to Genome: Unique*n_reads"] = merge_pv["Reads Mapped to Genome: Unique"]*merge_pv['Number of Reads']
     merge_pv["Reads Mapped to Genome: Unique+Multiple*n_reads"] = merge_pv["Reads Mapped to Genome: Unique+Multiple"]*merge_pv['Number of Reads']
     merge_pv["Q30 Bases in RNA read*n_reads"] = merge_pv["Q30 Bases in RNA read"]*merge_pv['Number of Reads']
     merge_pv["Q30 Bases in CB+UMI*n_reads"] = merge_pv["Q30 Bases in CB+UMI"]*merge_pv['Number of Reads']
     merge_pv["Reads With Valid Barcodes*n_reads"] = merge_pv["Reads With Valid Barcodes"]*merge_pv['Number of Reads']
-    Sequencing_Saturations_Total = 1-(merge_pv["yesUMIs"].sum()/merge_pv["yessubWLmatch_UniqueFeature"].sum())
-    Reads_mapped_Genome_unique=merge_pv["Reads Mapped to Genome: Unique*n_reads"].sum()/n_reads
-    Reads_mapped_Genome_unique_multi=merge_pv["Reads Mapped to Genome: Unique+Multiple*n_reads"].sum()/n_reads
-    Q30_RNA=merge_pv["Q30 Bases in RNA read*n_reads"].sum()/n_reads
-    Q30_CB_UMI=merge_pv["Q30 Bases in CB+UMI*n_reads"].sum()/n_reads
+    sequencing_saturations_total = 1-(merge_pv["yesUMIs"].sum()/merge_pv["yessubWLmatch_UniqueFeature"].sum())
+    reads_mapped_genome_unique=merge_pv["Reads Mapped to Genome: Unique*n_reads"].sum()/n_reads
+    reads_mapped_genome_unique_multi=merge_pv["Reads Mapped to Genome: Unique+Multiple*n_reads"].sum()/n_reads
+    q30_rna=merge_pv["Q30 Bases in RNA read*n_reads"].sum()/n_reads
+    q30_cb_umi=merge_pv["Q30 Bases in CB+UMI*n_reads"].sum()/n_reads
     valid_barcodes=merge_pv["Reads With Valid Barcodes*n_reads"].sum()/n_reads
     
     print("Calculating cell read metrics")
@@ -54,11 +54,11 @@ def merge_matrices(summary_file, align_file, cell_reads, counting_mode):
     reads_mapped_confidently_to_intronic_regions=cells["intronic"].sum()
     reads_mapped_confidently_to_transcriptome=cells["featureU"].sum()
     data = {"number_of_reads": [n_reads], 
-        "sequencing_saturation": [Sequencing_Saturations_Total], 
-        "fraction_of_unique_reads_mapped_to_genome": [Reads_mapped_Genome_unique],
-        "fraction_of_unique_and_multiple_reads_mapped_to_genome": [Reads_mapped_Genome_unique_multi],
-        "fraction_of_reads_with_Q30_bases_in_rna": [Q30_RNA],
-        "fraction_of_reads_with_Q30_bases_in_cb_and_umi": [Q30_CB_UMI],
+        "sequencing_saturation": [sequencing_saturations_total], 
+        "fraction_of_unique_reads_mapped_to_genome": [reads_mapped_genome_unique],
+        "fraction_of_unique_and_multiple_reads_mapped_to_genome": [reads_mapped_genome_unique_multi],
+        "fraction_of_reads_with_Q30_bases_in_rna": [q30_rna],
+        "fraction_of_reads_with_Q30_bases_in_cb_and_umi": [q30_cb_umi],
         "fraction_of_reads_with_valid_barcodes": [valid_barcodes],
         "reads_mapped_antisense_to_gene": [reads_mapped_antisense_to_gene],
         "reads_mapped_confidently_exonic": [reads_exonic],
