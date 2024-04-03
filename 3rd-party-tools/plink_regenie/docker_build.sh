0ad7d394c7be823fcd093a2b0de45049c83228be
aawdeh <awdeh@broadinstitute.org>
2024-04-03 05:21:36 -0400

    Add docker_build.sh file

---
+++ 3rd-party-tools/plink_regenie/docker_build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -e
+
+# Update version when changes to Dockerfile are made
+DOCKER_IMAGE_VERSION=1.0.0
+TIMESTAMP=$(date +"%s")
+DIR=$(cd $(dirname $0) && pwd)
+
+# Registries and tags
+GCR_URL="us.gcr.io/broad-gotc-prod/regenie-plink2"
+QUAY_URL="quay.io/broadinstitute/gotc-prod-regenie-plink2"
+
+# REGENIE version
+REGENIE_VERSION="2.0.2"
+
+# Necessary tools and help text
+TOOLS=(docker gcloud)
+HELP="$(basename "$0") [-h|--help] [-v|--version] [-t|tools] -- script to build the regenie-plink2 image and push to GCR & Quay
+
+where:
+    -h|--help Show help text
+    -v|--version Version of REGENIE (default: $REGENIE_VERSION) and plink2 to use 
+    -t|--tools Show tools needed to run script
+    "
+
+function main(){
+    for t in "${TOOLS[@]}"; do which "$t" >/dev/null || ok=no; done
+        if [[ $ok == no ]]; then
+            echo "Missing one of the following tools: "
+            for t in "${TOOLS[@]}"; do echo "$t"; done
+            exit 1
+        fi
+
+    while [[ $# -gt 0 ]]
+    do 
+    key="$1"
+    case $key in
+        -v|--version)
+        REGENIE_VERSION="$2"
+        shift
+        shift
+        ;;
+        -h|--help)
+        echo "$HELP"
+        exit 0
+        ;;
+        -t|--tools)
+        for t in "${TOOLS[@]}"; do echo "$t"; done
+        exit 0
+        ;;
+        *)
+        shift
+        ;;
+    esac
+    done
+    
+    IMAGE_TAG="$DOCKER_IMAGE_VERSION-$REGENIE_VERSION-$TIMESTAMP"
+
+    echo "building and pushing GCR Image - $GCR_URL:$IMAGE_TAG"
+    docker build --no-cache -t "$GCR_URL:$IMAGE_TAG" \
+        --build-arg REGENIE_VERSION="$REGENIE_VERSION" "$DIR" 
+    docker push "$GCR_URL:$IMAGE_TAG"
+
+    echo "tagging and pushing Quay Image"
+    docker tag "$GCR_URL:$IMAGE_TAG" "$QUAY_URL:$IMAGE_TAG"
+    docker push "$QUAY_URL:$IMAGE_TAG"
+
+    echo -e "$GCR_URL:$IMAGE_TAG" >> "$DIR/docker_versions.tsv"
+    echo "done"
+}
+
+main "$@"
