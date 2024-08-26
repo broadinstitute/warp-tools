0d76230626163131c08e289c30a90c95aeee5211
npetrill <npetrill@broadinstitute.org>
2024-08-26 11:35:09 -0400

    false to true

--- tools/scripts/sctools/setup.py
+++ tools/scripts/sctools/setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 setup(
     name="sctools",
-    use_scm_version=False,
+    use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="Utilities for large-scale distributed single cell" + "data processing",
     url="https://github.com/humancellatlas/sctools.git",
