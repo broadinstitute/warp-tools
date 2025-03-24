b4e8bf276d08f932cb8339b0f886d8e67faa8f94
Elizabeth Kiernan <55763654+ekiernan@users.noreply.github.com>
2025-03-24 08:48:39 -0400

    Update add_gene_name.py

--- 3rd-party-tools/build-indices/add_gene_name.py
+++ 3rd-party-tools/build-indices/add_gene_name.py
@@ -1,4 +1,3 @@
-# Script for adding gene_name as an additional attribute
 import re
 
 input_gtf = "genes.gtf"
@@ -17,16 +16,11 @@ with open(input_gtf, "r") as infile, open(output_gtf, "w") as outfile:
 
         attributes = fields[8]
 
-        # Extract gene value
-        match = re.search(r'gene\s+"([^"]+)"', attributes)
+        match = re.search(r'gene_id\s+"([^"]+)"', attributes)
         if match:
             gene_value = match.group(1)
-            # Only add gene_name if it doesn't already exist
             if 'gene_name' not in attributes:
-                attributes = attributes.replace(
-                    f'gene "{gene_value}";',
-                    f'gene "{gene_value}"; gene_name "{gene_value}";'
-                )
+                attributes = attributes.strip() + f' gene_name "{gene_value}";'
+
         fields[8] = attributes
         outfile.write("\t".join(fields) + "\n")
-
