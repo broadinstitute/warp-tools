bd85a7363dc8c5a5b1fac4a256146755197fee29
ekiernan <ekiernan@broadinstitute.org>
2025-03-24 10:00:55 -0400

    Update add_gene_name.py

--- 3rd-party-tools/build-indices/add_gene_name.py
+++ 3rd-party-tools/build-indices/add_gene_name.py
@@ -16,10 +16,15 @@ with open(input_gtf, "r") as infile, open(output_gtf, "w") as outfile:
 
         attributes = fields[8]
 
-        match = re.search(r'gene_id\s+"([^"]+)"', attributes)
-        if match:
-            gene_value = match.group(1)
-            if 'gene_name' not in attributes:
+        # Parse attributes into a dictionary
+        attr_dict = dict(re.findall(r'(\S+)\s+"(.*?)"', attributes))
+
+        # If gene_name is missing, try to add it
+        if "gene_name" not in attr_dict:
+            # Try to use 'gene' first, then 'gene_id', then 'transcript_id'
+            gene_value = attr_dict.get("gene") or attr_dict.get("gene_id") or attr_dict.get("transcript_id")
+            if gene_value:
+                # Add the gene_name at the end of the attributes string
                 attributes = attributes.strip() + f' gene_name "{gene_value}";'
 
         fields[8] = attributes
