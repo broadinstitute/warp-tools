0c3aaefa5b607af6ccd6946a2200329ed2ba1b79
Sid Cox <sidcox@gmail.com>
2026-08-29 16:32:49 -0400

    Disable -Werror in libStatGen for bookworm GCC 12

--- tools/fastqpreprocessing/fetch_and_make_dep_libs.sh
+++ tools/fastqpreprocessing/fetch_and_make_dep_libs.sh
@@ -3,6 +3,7 @@
 wget https://github.com/khajoue2/libStatGen/archive/refs/tags/v1.0.15.broad.tar.gz && \
 tar -zxvf v1.0.15.broad.tar.gz && \
 mv libStatGen-1.0.15.broad libStatGen && \
+sed -i 's/-Werror//g' libStatGen/general/Makefile && \
 make -C libStatGen && \
 
 wget https://github.com/kanedo/gzstream/archive/refs/heads/master.tar.gz -O gzstream.tar.gz && \
